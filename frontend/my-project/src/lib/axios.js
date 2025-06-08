5ff4c39a57e71432818e47ba74a50ba7c6bb6cee
prachi-stack <deopaprachi60@gmail.com>
2025-06-08 18:34:11 +0530

    Update axios.js

--- frontend/my-project/src/lib/axios.js
+++ frontend/my-project/src/lib/axios.js
@@ -1,6 +1,6 @@
 // setting up the axios default url
 import axios from 'axios'
 export const axiosInstance =axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://chatappbackend-7t5n.onrender.com/api",
     withCredentials:true
 })
