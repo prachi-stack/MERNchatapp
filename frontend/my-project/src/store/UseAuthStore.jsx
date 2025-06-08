46f8c3b0c6940ee2d766482a3d2dbbf16a6551ff
prachi-stack <deopaprachi60@gmail.com>
2025-06-08 18:43:14 +0530

    Update UseAuthStore.jsx

--- frontend/my-project/src/store/UseAuthStore.jsx
+++ frontend/my-project/src/store/UseAuthStore.jsx
@@ -4,7 +4,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chatappbackend-7t5n.onrender.com" : "/";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
